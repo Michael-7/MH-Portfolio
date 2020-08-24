36ff7aa813df44cbd0b4aad0255537421c52c041
Michael-7 <michael.m.heemskerk@gmail.com>
2020-08-25 06:49:24 +1200

    lottie player and desktop design

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 // function scrollTo() {
-	document.body.scrollTop = 1000;
+	// document.body.scrollTop = 1000;
 // };
 
 // scrollTo();
+
+var animation = bodymovin.loadAnimation({
+	container: document.getElementById('lottie'), // Required
+	path: './resources/lottie-leg.json', // Required
+	renderer: 'svg', // Required
+	loop: true, // Optional
+	autoplay: true, // Optional
+	name: "Hello World", // Name for future reference. Optional.
+  })
