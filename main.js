29c3b12078f421d33b7c66a47cf427d78be8a510
Michael-7 <michael.m.heemskerk@gmail.com>
2020-08-29 00:04:43 +1200

    landing-mobile-fix

--- main.js
+++ main.js
@@ -12,3 +12,12 @@ var animation = bodymovin.loadAnimation({
 	autoplay: true, // Optional
 	name: "Hello World", // Name for future reference. Optional.
 });
+
+var animation = bodymovin.loadAnimation({
+	container: document.getElementById('lottie-mob'), // Required
+	path: './resources/char-jump-coffee.json', // Required
+	renderer: 'svg', // Required
+	loop: false, // Optional
+	autoplay: true, // Optional
+	name: "Hello World", // Name for future reference. Optional.
+});
