c1d61d8ef44522ed231587142307dfc173a9fbdc
michael <michael.m.heemskerk@gmail.com>
2020-12-14 21:37:38 +0100

    Loading and Link animations

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 // Load functions
-scrollListener();
 loadAnimations();
+scrollListener();
 
 function scrollListener() {
 	el = document.getElementById('scroll-down-arrow');
@@ -12,6 +12,7 @@ function scrollListener() {
 };
 
 function loadAnimations() {
+	let loadingOverlay = document.getElementById('loading-overlay');
 	var charAnimationDesktop = bodymovin.loadAnimation({
 		container: document.getElementById('lottie-char'), // Required
 		path: './resources/char-jump-coffee.json', // Required
@@ -20,11 +21,14 @@ function loadAnimations() {
 		autoplay: false, // Optional
 	});
 
-	setTimeout(function(){ 
-		document.getElementById('loading-overlay').style.display = 'none';
+	charAnimationDesktop.addEventListener('DOMLoaded', () => {
+		loadingOverlay.classList.add('animate');
+	});
+
+	loadingOverlay.addEventListener('animationend', () => {
 		charAnimationDesktop.play();
-	}, 2000);
-	
+		document.getElementById('loading-overlay').style.display = 'none';
+	});
 
 	var charAnimationMobile = bodymovin.loadAnimation({
 		container: document.getElementById('lottie-mob'), // Required
